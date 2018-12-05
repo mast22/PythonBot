af5d55ec720172fba1c7370a573701f56334d297
Nick <kojenin99@gmail.com>
2018-12-05 17:47:46 +0300

    db acceess

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import telebot
 import os
 import const
+from tinydb import TinyDB, Query
+import datetime
+import time
+import json
+
+db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
 
 token = os.environ.get('TOKEN')
 
@@ -21,6 +27,17 @@ main_menu_markup.row(participants, organisers)
 def start_handler(m):
     cid = str(m.chat.id)
     bot.send_message(cid, const.greeting, reply_markup=main_menu_markup)
+    User = Query()
+    t = datetime.datetime.now()
+    if not db.get(User.cid == cid):
+        db.insert(
+            {
+                'cid': cid,
+                'date': '{} {}:{}'.format(t.day, t.hour, t.minute),
+                'username': m.from_user.username,
+                'first_name': m.from_user.first_name or 'without name'
+            }
+        ) 
 
 @bot.message_handler(func=lambda m: m.text == register)
 def register_handler(m):
@@ -51,6 +68,17 @@ def organisers_handler(m):
     bot.send_message(cid, const.organisers)
     bot.send_message(cid, const.contacts, reply_markup=main_menu_markup)
 
+@bot.message_handler(func=lambda m: m.text == 'get_logs')
+def logs_handler(m):
+    cid = str(m.chat.id)
+    users = db.all()
+    message = ''
+    for i in users:
+        m = i['cid'] + '\n' + i['first_name'] + '\n' + i['username'] + '\n' + i['date'] + '\n' + '-----------------' + '\n'
+        message += m
+    bot.send_message(cid, message)
+
+
 @bot.message_handler()
 def else_handler(m):
     bot.send_message(m.chat.id, 'Пожалуйста, используйте клавиатуру', reply_markup=main_menu_markup)
@@ -59,4 +87,5 @@ while True:
     try:
         bot.polling(none_stop=True)
     except Exception as err:
-        time.sleep(5)
+        print(err)
+        time.sleep(2)
