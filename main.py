43f91fbc814854bdc1a6081d0aad0249fdc83e45
Nick <kojenin99@gmail.com>
2019-01-03 13:39:42 +0300

    var rename + .gitignore changes

--- main.py
+++ main.py
@@ -12,16 +12,16 @@ token = os.environ.get('TOKEN')
 
 bot = telebot.TeleBot(token)
 
-time_table = 'Расписание'
-about = 'О мероприятии'
-participants = 'Участники'
-organisers = 'Организаторы'
-register = 'Регистрация'
+TIME_TABLE = 'Расписание'
+ABOUT = 'О мероприятии'
+PARTICIPANTS = 'Участники'
+ORGANISERS = 'Организаторы'
+REGISTER = 'Регистрация'
 
 main_menu_markup = telebot.types.ReplyKeyboardMarkup(True)
-main_menu_markup.row(register)
-main_menu_markup.row(time_table, about)
-main_menu_markup.row(participants, organisers)
+main_menu_markup.row(REGISTER)
+main_menu_markup.row(TIME_TABLE, ABOUT)
+main_menu_markup.row(PARTICIPANTS, ORGANISERS)
 
 @bot.message_handler(commands=['start'])
 def start_handler(m):
@@ -39,30 +39,30 @@ def start_handler(m):
             }
         ) 
 
-@bot.message_handler(func=lambda m: m.text == register)
+@bot.message_handler(func=lambda m: m.text == REGISTER)
 def register_handler(m):
     cid = str(m.chat.id)
     bot.send_message(cid, const.register)
     bot.send_message(cid, const.register_link, reply_markup=main_menu_markup)
 
-@bot.message_handler(func=lambda m: m.text == time_table)
+@bot.message_handler(func=lambda m: m.text == TIME_TABLE)
 def time_table_handler(m):
     cid = str(m.chat.id)
     bot.send_message(cid, const.time_table_text_1, parse_mode='HTML')
     bot.send_message(cid, const.time_table_text_2, reply_markup=main_menu_markup, parse_mode='HTML')
 
-@bot.message_handler(func=lambda m: m.text == about)
+@bot.message_handler(func=lambda m: m.text == ABOUT)
 def about_handler(m):
     cid = str(m.chat.id)
     bot.send_message(cid, const.about, reply_markup=main_menu_markup)
     bot.send_location(cid, const.loc_lat, const.loc_long)
 
-@bot.message_handler(func=lambda m: m.text == participants)
+@bot.message_handler(func=lambda m: m.text == PARTICIPANTS)
 def participants_handler(m):
     cid = str(m.chat.id)
     bot.send_message(cid, const.participants, reply_markup=main_menu_markup)
 
-@bot.message_handler(func=lambda m: m.text == organisers)
+@bot.message_handler(func=lambda m: m.text == ORGANISERS)
 def organisers_handler(m):
     cid = str(m.chat.id)
     bot.send_message(cid, const.organisers)
